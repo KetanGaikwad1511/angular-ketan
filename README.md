8b247da2a462ef355b61b8db6023718d1fbd918a
KetanGaikwad1511 <ketangaikwad2118@gmail.com>
2022-03-27 09:46:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-sq7v1b
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-sq7v1b)
---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, of, timer } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+
+ observable = new Observable((observer)=>{
+  observer.next('first item');
+  observer.next('second item');
+  observer.next('third item');
+  observer.complete();
+});
+
+  ngOnInit(): void {
+   
+
+    this.observable.subscribe(res=>
+      {
+        console.log(res)
+      });
+
+this.calculateNumbers()
+    
+  }
+  name = 'Angular';
+
+  getNumbers(): Observable<number> {
+    return of(1, 2, 3, 4, 5, 6, 7);
+  } 
+  calculateNumbers() {
+    this.getNumbers()
+      .pipe(
+        filter((n) => n % 2 === 1),
+        map((n) => n + 10)
+      )
+      .subscribe((result) => console.log(result));
+  }
+}

2264035a513e676a066dadaae96c7abf05b68677
KetanGaikwad1511 <ketangaikwad2118@gmail.com>
2022-04-03 12:54:18 +0530

    2 files added

---
+++ src/app/ng-on-change-life-cycle/ng-on-change-life-cycle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-on-change-life-cycle',
+  templateUrl: './ng-on-change-life-cycle.component.html',
+  styleUrls: ['./ng-on-change-life-cycle.component.css']
+})
+export class NgOnChangeLifeCycleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

52ed911ac4fda4cb64d5a69f469f4e7278941709
KetanGaikwad1511 <ketangaikwad2118@gmail.com>
2022-04-03 13:04:00 +0530

    1 file modified

--- src/app/ng-on-change-life-cycle/ng-on-change-life-cycle.component.ts
+++ src/app/ng-on-change-life-cycle/ng-on-change-life-cycle.component.ts
@@ -12,4 +12,14 @@ export class NgOnChangeLifeCycleComponent implements OnInit {
   ngOnInit() {
   }
 
+  //this is chikd componnet that recieves data from the parent component and 
+  //will get fired in ngOnChange Hook life cycle..
+  //ngOnChange life cycle will get initialized first...
+  // @Input will be used in ngOnChnage life cycle hook...
+  // @input will be placed inside child component
+  // and same reference will bind in the html selector of the child component
+  // and that selector will be called in the parent component 
+  // this will share data between parant to child component...
+  
+
 }

aa4d716e220acdae1598e44f829ffdfe7856ed58
KetanGaikwad1511 <ketangaikwad2118@gmail.com>
2022-04-03 13:05:22 +0530

    1 file modified

--- src/app/ng-on-change-life-cycle/ng-on-change-life-cycle.component.ts
+++ src/app/ng-on-change-life-cycle/ng-on-change-life-cycle.component.ts
@@ -3,23 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ng-on-change-life-cycle',
   templateUrl: './ng-on-change-life-cycle.component.html',
-  styleUrls: ['./ng-on-change-life-cycle.component.css']
+  styleUrls: ['./ng-on-change-life-cycle.component.css'],
 })
 export class NgOnChangeLifeCycleComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
-  //this is chikd componnet that recieves data from the parent component and 
+  //this is child componnet that recieves data from the parent component and
   //will get fired in ngOnChange Hook life cycle..
-  //ngOnChange life cycle will get initialized first...
+  //ngOnChange life cycle will get initialized first before ngOnIt(it will  //used for the initialization of the data)...
   // @Input will be used in ngOnChnage life cycle hook...
   // @input will be placed inside child component
   // and same reference will bind in the html selector of the child component
-  // and that selector will be called in the parent component 
+  // and that selector will be called in the parent component
   // this will share data between parant to child component...
-  
-
 }

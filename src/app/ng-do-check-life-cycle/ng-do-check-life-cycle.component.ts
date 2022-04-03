3ee9f61c03c7acd81d059d1d0d2ba02b9d7e39f4
KetanGaikwad1511 <ketangaikwad2118@gmail.com>
2022-04-03 13:33:38 +0530

    1 file modified, 2 added

---
+++ src/app/ng-do-check-life-cycle/ng-do-check-life-cycle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-do-check-life-cycle',
+  templateUrl: './ng-do-check-life-cycle.component.html',
+  styleUrls: ['./ng-do-check-life-cycle.component.css']
+})
+export class NgDoCheckLifeCycleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
